351c6c1dba4de15ce0cf3b59e9ca0dbd37942fff
Matt <killzone207@gmail.com>
2015-04-17 19:33:55 -0400

    Implemented Deg_Rad for 2D Lists

--- nao.py
+++ nao.py
@@ -5,8 +5,8 @@ Created on Mon Apr 06 18:42:35 2015
 @author: killz_000
 """
 import sys
+import math
 #import random
-import naoqi
 from naoqi import ALProxy
 
 #%%
@@ -18,18 +18,35 @@ Block = 3
 LUppercut = 4
 RUppercut = 5
 
+MaxArmStates = 6
+MaxLegStates = 2
+MaxJoints = 6
+
 Combo = [[Stance, LJab, Stance, RJab], [Stance, LJab, RJab], [Stance, RJab, LUppercut], [Stance, LUppercut, RUppercut], [Stance, LJab, RJab, Block], [Stance, LJab, RJab, Block, RJab, LJab, Block]]
 
 LArm = ["LShoulderPitch", "LShoulderRoll", "LElbowYaw", "LElbowRoll", "LWristYaw", "LHand"]
-LArmAngles = [[100, 0.5, -90, -88.5, -90], [-15, 0.5, -0.5, -2, 0], [114.5, 0.5, -90, -88.5, -90], [-10, 0.5, -55, -75, -90], [-20, 0.5, -90, -45, -90], [114.5, 0.5, -98, -88.5, -90]]
+LArmAngles = [[100.0, 0.5, -90.0, -88.5, -90.0, 0.0], [-15.0, 0.5, -0.5, -2.0, 0.0, 0.0], [114.5, 0.5, -90.0, -88.5, -90.0, 0.0], [-10.0, 0.5, -55.0, -75.0, -90.0, 0.0], [-20.0, 0.5, -90.0, -45.0, -90.0, 0.0], [114.5, 0.5, -98.0, -88.5, -90.0, 0.0]]
 RArm = ["RShoulderPitch", "RShoulderRoll", "RElbowYaw", "RElbowRoll", "RWristYaw", "RHand"]
-RArmAngles = [[100, -0.5, 90, 88.5, 90], [114.5, -0.5, 98, 88.5, 90], [-15, -0.5, 0.5, 2, 0], [-10, -0.5, 55, 75, 90], [114.5, -0.5, 98, 88.5, 90], [-20, -0.5, 90, 45, 90]]
+RArmAngles = [[100.0, -0.5, 90.0, 88.5, 90.0, 0.0], [114.5, -0.5, 98.0, 88.5, 90.0, 0.0], [-15.0, -0.5, 0.5, 2.0, 0.0, 0.0], [-10.0, -0.5, 55.0, 75.0, 90.0, 0.0], [114.5, -0.5, 98.0, 88.5, 90.0, 0.0], [-20.0, -0.5, 90.0, 45.0, 90.0, 0.0]]
 LLeg = ["LHipYawPitch", "LHipPitch", "LHipRoll", "LKneePitch", "LAnklePitch", "LAnkleRoll"]
-LLegAngles = [[0, -50, 2.5, 80, -36, -3], [0, 0, 0, 0, 0, 0]]
+LLegAngles = [[0.0, -50.0, 2.5, 80.0, -36.0, -3.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
 RLeg = ["RHipYawPitch", "RHipPitch", "RHipRoll", "RKneePitch", "RAnklePitch", "RAnkleRoll"]
-RLegAngles = [[0, -50, -2.5, 80, -36, 3], [0, 0, 0, 0, 0, 0]]
+RLegAngles = [[0.0, -50.0, -2.5, 80.0, -36.0, 3.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
 
 fractionMaxSpeed = 0.2
+
+#%% convert 2d arrays from deg to rad
+def Deg_Rad(x, states, joints):
+    for i in range(states):
+        for j in range(joints):
+            x[i][j] = math.radians(x[i][j])
+
+Deg_Rad(LArmAngles, MaxArmStates, MaxJoints)
+Deg_Rad(RArmAngles, MaxArmStates, MaxJoints)
+Deg_Rad(LLegAngles, MaxLegStates, MaxJoints)
+Deg_Rad(RLegAngles, MaxLegStates, MaxJoints)
+
+
 
 #%%
 def StiffnessOn(proxy):
@@ -66,21 +83,23 @@ def main(robotIP):
     id = motion.setAngles(RLeg, RLegAngles[0], fractionMaxSpeed)
     motion.wait(id, 0)
     
-    for i in range(len(Combo[0])):
-        for move in Combo:
-            motion.setAngles(LArm, LArmAngles[move[i]], fractionMaxSpeed)
-            id = motion.setAngles(RArm, RArmAngles[move[i]], fractionMaxSpeed)
-            motion.wait(id, 0)
+#    for i in range(len(Combo[0])):
+#        for move in Combo:
+#            motion.setAngles(LArm, LArmAngles[move[i]], fractionMaxSpeed)
+#            id = motion.setAngles(RArm, RArmAngles[move[i]], fractionMaxSpeed)
+#            motion.wait(id, 0)
     
-    #motion.setAngles(LArm, LArmAngles[Block], fractionMaxSpeed)
+#    motion.setAngles(LArm, LArmAngles[Block], fractionMaxSpeed)
     
 
 if __name__ == "__main__":
-    robotIp = "169.254.246.148"
+    robotIp = "169.254.226.148"
 
     if len(sys.argv) <= 1:
-        print "Usage python motiuon_poseInit.pyrobotIP (optional default: 127.0.0.1)"
+        print "Connecting to IP: 169.254.226.148"
     else:
         robotIp = sys.argv[1]
         
     main(robotIp)
+    
+#%%
